c52ce0512ee782942c959555fc7fcee69193d4ea
jinzhu <simpleease@163.com>
2023-04-01 17:02:25 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'normal_animate_widget.dart';
+import 'builder_animate_widget.dart';
+import 'widget_animate_widget.dart';
+import 'hero_transition.dart';
+
+int syncFactorial(n) => n < 2 ? n : n * syncFactorial(n-1);
+
+void main() async {
+  var f = (compute(syncFactorial, 4));
+  print("${await f}");
+  print("after compute");
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        body: TabBarView(
+          children: [
+            NormalAnimateWidget(),
+            BuilderAnimateWidget(),
+            WidgetAnimateWidget(),
+            Page1()
+          ],
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.home),text: "普通动画",),
+            Tab(icon: Icon(Icons.rss_feed),text: "Builder动画",),
+            Tab(icon: Icon(Icons.perm_identity),text: "Widget动画",),
+            Tab(icon: Icon(Icons.message),text:'hero动画')
+          ],
+          unselectedLabelColor: Colors.blueGrey,
+          labelColor: Colors.blue,
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
